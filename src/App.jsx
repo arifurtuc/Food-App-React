7a91f815713eefa6e6fc89c7048d110fdc73c954
arif <arifur.tuc@gmail.com>
2024-02-15 23:06:58 +0100

    Creating nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import RecipeList from "./components/RecipeList";
+import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
   const [recipeData, setRecipeData] = useState([]);
   return (
     <>
+      <Nav />
       <Search recipeData={recipeData} setRecipeData={setRecipeData} />
       <RecipeList recipeData={recipeData} />
     </>
